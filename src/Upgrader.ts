c5f24a18e6d3bf85194e7d9ee61602ed2df2dd00
lennytmp <lennytmp@gmail.com>
2017-10-19 00:36:30 +0100

    stop higlightin paths to lighten client load

--- src/Upgrader.ts
+++ src/Upgrader.ts
@@ -8,13 +8,13 @@ export function run(creep: Creep): void {
   if (creep.memory.upgrading) {
     if (creep.room.controller && 
         creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+      creep.moveTo(creep.room.controller);
     }
   }
   else {
     var spawner = Game.spawns['Spawn1'];
     if (creep.withdraw(spawner, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(spawner, {visualizePathStyle: {stroke: '#ffaa00'}});
+      creep.moveTo(spawner);
     }
   }
 }
