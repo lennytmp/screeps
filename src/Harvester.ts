0f6004655750d089a6b3e507e9c505fd19558c05
lennytmp <lennytmp@gmail.com>
2017-10-18 21:26:41 +0100

    increase time for path reuse

--- src/Harvester.ts
+++ src/Harvester.ts
@@ -2,7 +2,7 @@ export function run(creep: Creep, src: string): void {
   if (creep.carry.energy! < creep.carryCapacity) {
     let source = <Source>Game.getObjectById(src);
     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(source);
+      creep.moveTo(source, {reusePath:100});
     }
   } else {
     if(creep.transfer(Game.spawns['Spawn1']!, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
