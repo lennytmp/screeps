0108b2e07f7c04b3eb44ef3739911919aa2d47e5
Jille Timmermans <jille@quis.cx>
2017-10-22 00:18:54 +0100

    Minor fixes to Fighter

--- src/Fighter.ts
+++ src/Fighter.ts
@@ -1,16 +1,12 @@
 export function run(creep: Creep): void {
-  let enemies = <Creep[]>creep.room.find(FIND_HOSTILE_CREEPS);
   let isShooting = false;
-  _.forEach(<Creep[]>creep.room.find(FIND_HOSTILE_CREEPS),
-            function(enemy: Creep) {
+  let enemies = <Creep[]>creep.room.find(FIND_HOSTILE_CREEPS);
+  _.forEach(enemies, function(enemy: Creep) {
     if(creep.rangedAttack(enemy) != ERR_NOT_IN_RANGE) {
       isShooting = true;
     }
   });
   if (!isShooting) {
-    creep.move(Math.round(Math.random()*8)); // Simliest patrol
+    creep.move(Math.floor(Math.random()*8)); // Simpliest patrol
   }
 }
-
-
-
