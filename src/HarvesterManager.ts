ec82a3ae2a14819c891773cfe54b8f74fbe0795a
Jille Timmermans <jille@quis.cx>
2017-10-21 19:07:17 +0100

    Convert tabs to spaces :(

--- src/HarvesterManager.ts
+++ src/HarvesterManager.ts
@@ -3,8 +3,8 @@ import * as Fmngr from "./FighterManager";
 import * as Harvester from "./Harvester";
 
 export interface SourceDefinition {
-	id: string,
-	miningPositions: [number, number][]
+  id: string,
+  miningPositions: [number, number][]
 }
 
 export class HarvesterManager extends Manager.Manager {
@@ -12,26 +12,26 @@ export class HarvesterManager extends Manager.Manager {
   readonly role = 'harvester';
 
   commandMinions(): void {
-		let needs: { [id: string]: number; } = {};
-		_.forEach(Memory.harvester.sources, function(s: SourceDefinition) {
-			needs[s.id] = s.miningPositions.length;
-		});
-    _.forEach(this.minions, function(minion: Creep) {
-			if(minion.memory.source) {
-				needs[minion.memory.source]--;
-			}
+    let needs: { [id: string]: number; } = {};
+    _.forEach(Memory.harvester.sources, function(s: SourceDefinition) {
+      needs[s.id] = s.miningPositions.length;
     });
     _.forEach(this.minions, function(minion: Creep) {
-			if(!minion.memory.source) {
-				_.forEach(needs, function(need: number, src: string): boolean {
-					if(need > 0) {
-						minion.memory.source = src;
-						return false;
-					}
-					return true;
-				});
-			}
-			Harvester.run(minion, minion.memory.source);
+      if(minion.memory.source) {
+        needs[minion.memory.source]--;
+      }
+    });
+    _.forEach(this.minions, function(minion: Creep) {
+      if(!minion.memory.source) {
+        _.forEach(needs, function(need: number, src: string): boolean {
+          if(need > 0) {
+            minion.memory.source = src;
+            return false;
+          }
+          return true;
+        });
+      }
+      Harvester.run(minion, minion.memory.source);
     });
   }
 
@@ -43,16 +43,16 @@ export class HarvesterManager extends Manager.Manager {
       return res;
     }
 
-		if(!Memory.harvester) {
-			Memory.harvester = {};
-		}
-		if(!Memory.harvester.sources) {
-			Memory.harvester.sources = this.calcSources(Game.spawns['Spawn1'].room);
-		}
-		let needed = 0;
-		_.forEach(Memory.harvester.sources, function(s: SourceDefinition) {
-			needed += s.miningPositions.length;
-		});
+    if(!Memory.harvester) {
+      Memory.harvester = {};
+    }
+    if(!Memory.harvester.sources) {
+      Memory.harvester.sources = this.calcSources(Game.spawns['Spawn1'].room);
+    }
+    let needed = 0;
+    _.forEach(Memory.harvester.sources, function(s: SourceDefinition) {
+      needed += s.miningPositions.length;
+    });
 
     let parts = this.getBodyParts(minBodyParts, currentEnergy);
     if (parts.length == 0) {
@@ -68,31 +68,31 @@ export class HarvesterManager extends Manager.Manager {
     return res;
   }
 
-	calcSources(room: Room): SourceDefinition[] {
-		let res: SourceDefinition[] = [];
+  calcSources(room: Room): SourceDefinition[] {
+    let res: SourceDefinition[] = [];
     let sources = <Source[]>room.find(FIND_SOURCES);
     _.forEach(sources, function(src: Source) {
       if (!Fmngr.FighterManager.isSafePos(src.pos)) {
         return true;
       }
-			let p = src.pos;
-			let positions: [number, number][] = [];
-			let area = room.lookForAtArea(LOOK_TERRAIN, p.y-1, p.x-1, p.y+1, p.x+1, false);
-			_.forEach(area, function(tmp: LookAtResultMatrix, y: any) {
-				_.forEach(tmp, function(what: string, x: any) {
-					if(what == "plain") {
-						positions.push([x, y]);
-					}
-				});
-			});
-			res.push({
-				"id": src.id,
-				"miningPositions": positions
-			});
+      let p = src.pos;
+      let positions: [number, number][] = [];
+      let area = room.lookForAtArea(LOOK_TERRAIN, p.y-1, p.x-1, p.y+1, p.x+1, false);
+      _.forEach(area, function(tmp: LookAtResultMatrix, y: any) {
+        _.forEach(tmp, function(what: string, x: any) {
+          if(what == "plain") {
+            positions.push([x, y]);
+          }
+        });
+      });
+      res.push({
+        "id": src.id,
+        "miningPositions": positions
+      });
       return true;
-		});
-		return res;
-	}
+    });
+    return res;
+  }
 
   static getMyConsumer(creep: Creep): Creep | Structure {
     return creep.pos.findClosestByRange(
