cdcf4cece0068387269904b0f69af96b5ac43f9f
Jille Timmermans <jille@quis.cx>
2017-10-22 00:10:16 +0100

    Make harvesters a little less stupid

--- src/Harvester.ts
+++ src/Harvester.ts
@@ -1,7 +1,12 @@
 import * as Hmngr from "./HarvesterManager";
 
 export function run(creep: Creep, src: string): void {
-  if (creep.carry.energy! < creep.carryCapacity) {
+  if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+    creep.memory.harvesting = false;
+  } else if (!creep.memory.harvesting && creep.carry.energy == 0) {
+    creep.memory.harvesting = true;
+  }
+  if (creep.memory.harvesting) {
     let source = <Source>Game.getObjectById(src);
     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
       creep.moveTo(source);
