8fe2833663d2bc1780d8076efc9f0113fd948f6a
lennytmp <lennytmp@gmail.com>
2017-10-28 17:52:28 +0100

    Harvestor bug fix

--- src/Harvester.ts
+++ src/Harvester.ts
@@ -5,22 +5,25 @@ import * as H from "./Harvester";
 export class Harvester {
 
   creep: Creep;
-  isHarvesting: boolean = true;
 
   constructor(creep: Creep) {
     this.creep = creep;
     if (creep.memory.isReviving && creep.ticksToLive > 1000) {
       creep.memory.isReviving = false;
-      this.isHarvesting = true;
+      creep.memory.isHarvesting = true;
       return;
     }
     if ((creep.ticksToLive < 300 && Utils.shouldBeRenewed(creep)) || creep.memory.isReviving) {
       creep.memory.isReviving = true;
-      this.isHarvesting = false;
+      creep.memory.isHarvesting = false;
       return;
     }
-    if (this.isHarvesting && creep.carry.energy == creep.carryCapacity) {
-      this.isHarvesting = false;
+    if (creep.memory.isHarvesting && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.isHarvesting = false;
+      return;
+    }
+    if (!creep.memory.isHarvesting  && creep.carry.energy == 0) {
+      creep.memory.isHarvesting = true;
     }
   }
 
@@ -29,7 +32,7 @@ export class Harvester {
     if (creep.memory.isReviving) {
       creep.moveTo(Game.spawns['Spawn1']);
     }
-    if (this.isHarvesting) {
+    if (creep.memory.isHarvesting) {
       let source = <Source>Game.getObjectById(src);
       if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
@@ -37,7 +40,7 @@ export class Harvester {
     }
     // Always try to transfer into our consumer.
     if (creep.transfer(dst, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-      if (!this.isHarvesting) {
+      if (!creep.memory.isHarvesting) {
         // But only move if we're full (or were full and haven't dropped everything yet)
         creep.moveTo(dst);
       }
